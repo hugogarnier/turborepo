01648e61ed39ef7f82228a74852ae0757fe5ad19
GARNIER Hugo <hgarnier@meilleurtaux.com>
2022-05-24 15:28:12 +0200

    init turborepo with tailwind

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,21 @@
+import Head from "next/head";
 import { Button } from "ui";
 
-export default function Docs() {
+const Doc = () => {
   return (
-    <div>
-      <h1>Docs</h1>
-      <Button />
-    </div>
+    <>
+      <Head>
+        <title>Doc App</title>
+      </Head>
+
+      <div className="flex w-full min-h-screen justify-center items-center">
+        <div className="flex space-x-3 items-center">
+          <h1 className="font-bold text-lg">Doc</h1>
+          <Button />
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Doc;
