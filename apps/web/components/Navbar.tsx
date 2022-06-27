d2b755e1d7187bd8eeb33738748ed940fb0a314e
Hugo Garnier <hugo.garnier@ik.me>
2022-06-27 17:05:39 +0200

    edit nav

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
       <div className="max-w-screen-xl p-4 mx-auto">
         <div className="flex items-center justify-between space-x-4 lg:space-x-10">
           <div className="flex lg:w-0 lg:flex-1">
-            <span className="w-20 h-10 bg-gray-200 rounded-lg"></span>
+            <a className="p-2 bg-gray-200 rounded-lg text-gray-500 hover:text-gray-700 text-xl" href="">H.G</a>
           </div>
 
           <nav className="hidden space-x-8 text-xl font-medium md:flex">
@@ -17,27 +17,29 @@ export const Navbar = () => {
               className="text-gray-500 hover:text-amber-600 transition-all duration-100"
               href=""
             >
-              Contact
+              Projects
             </a>
             <a
               className="text-gray-500 hover:text-amber-600 transition-all duration-100"
               href=""
             >
-              Blog
+              About
             </a>
           </nav>
 
-          <div className="items-center justify-end flex-1 hidden space-x-4 sm:flex">
-            <Button variant="primary">test</Button>
-            <Button variant="secondary">tessdqsdqsdt</Button>
+          <div className="items-center justify-end flex-1 hidden space-x-4 md:flex">
+
+            <a
+              className="inline-block px-4 py-2 text-xl font-medium text-white transition bg-indigo-600 rounded hover:scale-110 hover:rotate-2 active:bg-indigo-500 focus:outline-none focus:ring"
+              href="">
+              Stay in touch
+            </a>
           </div>
 
-          <div className="md:hidden">
-            <button
-              className="p-2 text-gray-600 bg-gray-100 rounded-lg"
-              type="button"
+          <details className="md:hidden group">
+            <summary
+              className="flex items-center p-2 text-gray-600 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200 hover:text-gray-700"
             >
-              <span className="sr-only">Open menu</span>
               <svg
                 aria-hidden="true"
                 className="w-5 h-5"
@@ -53,8 +55,29 @@ export const Navbar = () => {
                   strokeWidth="2"
                 />
               </svg>
-            </button>
-          </div>
+            </summary>
+
+            <nav className="absolute top-20 left-0 m-auto flex flex-col justify-start items-center
+        bg-white h-screen w-screen my-8">
+              <a
+                href=""
+                className="flex items-center my-4 text-gray-500 hover:text-gray-700"
+              >
+                <span className="ml-3 text-5xl font-medium">Projects</span>
+              </a>
+              <a
+                href=""
+                className="flex items-center my-4 text-gray-500 hover:text-gray-700"
+              >
+                <span className="ml-3 text-5xl font-medium">About</span>
+              </a>
+              <a
+                className="inline-block my-4 px-4 py-2 text-5xl font-medium text-white transition bg-indigo-600 rounded hover:scale-110 hover:rotate-2 active:bg-indigo-500 focus:outline-none focus:ring"
+                href="">
+                Stay in touch
+              </a>
+            </nav>
+          </details>
         </div>
       </div>
     </header>
